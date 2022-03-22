17dfdf2fa331339c7003d08a9ee8444cbe0210d1
Vinnie <vinnie.terlouw@gmail.com>
2022-03-22 10:08:41 +0100

    fixed text being through eachother

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -134,7 +134,7 @@ export default function Homepage() {
               <select
                 class="form-select appearance-none
       block
-      w-40
+      w-20
       px-3
       py-1.5
       text-base
